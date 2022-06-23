638326797a1244be70ff2699af8c75f6868a04a0
Peter <lacazepk@gmail.com>
2022-06-23 09:29:21 +0100

    changed robot IP to correct value

--- iiwa_bringup/launch/iiwa.launch.py
+++ iiwa_bringup/launch/iiwa.launch.py
@@ -95,7 +95,7 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             'robot_ip',
-            default_value='172.31.1.138 ',
+            default_value='172.31.1.10',
             description='Robot IP of FRI interface',
         )
     )
